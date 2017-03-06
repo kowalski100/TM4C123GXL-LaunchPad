46a43d1c17cdea29da4391cfb5bf4146cd76b9c0
Ijaz Ahmad <ijaz.a100@yahoo.com>
2017-03-05 23:20:55 -0800

    formating text

--- PLL/PLL.c
+++ PLL/PLL.c
@@ -1,7 +1,7 @@
 
 /*********************************************************************
 
-			Tiva LaunchPad (TM4C123GH6PM) Bare Matel Programming Demo
+       Tiva LaunchPad (TM4C123GH6PM) Bare Matel Programming Demo
 
                       PLL (Phase Lockup Loop) 
                         
@@ -72,14 +72,14 @@ void toggleLED(void) {
   SYSCTL->GPIOHBCTL       &= ~(1UL << 4);   //Use APB bus for GPIOF
   SYSCTL->RCGC2           |= 0x020;         //enable clock to GPIOF
 	delay(1);                                 //allow clock to stablize
-	GPIOF->LOCK		          = 0x4C4F434B;     //Unlock GPIOCR Register for write access
-	GPIOF->CR			          = 0x1F;           //Unlock GPIOAFSEL, GPIOPUR, GPIOPDR, or GPIODEN bits to be written
-	GPIOF->DIR 		          = 0x0E;           //PF3-1	Output, PF0,PF4 Input
-	GPIOF->AFSEL	          = 0x00;           //Disable alternate function on all PORTF pins
-	GPIOF->PCTL 	          = 0x00000000;     //GPIO function on PCTL
-	GPIOF->PUR 		          = 0x1F;           //Pullup all Pins
-	GPIOF->DEN		          = 0x1F;           //Enable Digital I/O function of all PORTF pins
-	GPIOF->AMSEL	          = 0x00;           //Disable analogue functions on PORTF pins
+	GPIOF->LOCK             = 0x4C4F434B;     //Unlock GPIOCR Register for write access
+	GPIOF->CR               = 0x1F;           //Unlock GPIOAFSEL, GPIOPUR, GPIOPDR, or GPIODEN bits to be written
+	GPIOF->DIR              = 0x0E;           //PF3-1	Output, PF0,PF4 Input
+	GPIOF->AFSEL            = 0x00;           //Disable alternate function on all PORTF pins
+	GPIOF->PCTL             = 0x00000000;     //GPIO function on PCTL
+	GPIOF->PUR              = 0x1F;           //Pullup all Pins
+	GPIOF->DEN              = 0x1F;           //Enable Digital I/O function of all PORTF pins
+	GPIOF->AMSEL            = 0x00;           //Disable analogue functions on PORTF pins
   while (1) {
     GPIOF->DATA = 0x00;
 		delay(1000);
