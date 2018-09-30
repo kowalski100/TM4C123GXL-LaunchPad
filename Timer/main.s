96bdcb8a85b63b805a1e26bc87a94f49801bc73c
kowalski100 <ijaz.a100@yahoo.com>
2018-09-29 23:36:21 -0700

    Timer project updatation

--- Timer/main.s
+++ Timer/main.s
@@ -1,9 +1,8 @@
-
 ;	@author:      Ijaz Ahmad
 ;
 ;	@warranty:	  void
 ;
-;	@description: Accurate 500ms delay using Timers
+;	@description: Accurate 1000ms delay using Timers
 ;
 
 
@@ -23,20 +22,20 @@ RCGC2GPIO_REG               EQU	        0x400FE108
 GPIOHBCTL_REG               EQU	        0x400FE06C
 
 ; gpio registers
-GPIOFDIR_APB_REG            EQU	        0x40025400
-GPIOFDEN_APB_REG            EQU	        0x4002551C
-GPIOFDATA_APB_REG           EQU	        0x40025008
-GPIOFLOCK_APB_REG           EQU	        0x40025520
-GPIOFCR_APB_REG	            EQU	        0x40025524
-GPIOFPUR_APB_REG            EQU	        0x40025510
-GPIOFPDR_APB_REG            EQU	        0x40025514
+GPIOFDIR_APB_REG            EQU         0x40025400
+GPIOFDEN_APB_REG            EQU         0x4002551C
+GPIOFDATA_APB_REG           EQU         0x40025008
+GPIOFLOCK_APB_REG           EQU         0x40025520
+GPIOFCR_APB_REG             EQU         0x40025524
+GPIOFPUR_APB_REG            EQU         0x40025510
+GPIOFPDR_APB_REG            EQU         0x40025514
 
 ;NVIC Registers
 NVIC_EN0_REG                EQU         0xE000E100
 
 ; register values
 GPIO_UNLOCK_VAL				EQU			0x4C4F434B
-TIMER_DELAY_VAL             EQU         16000000
+TIMER_DELAY_VAL             EQU         16000000-1
 
 
 				AREA 	|.text|, CODE, READONLY, ALIGN=2
@@ -55,34 +54,34 @@ Main
 __timer_init
                 
                 ;enable clock to Timer-0
-				LDR		R1,		=RCGCTIMER_REG
-				LDR		R0,		[R1]
-				ORR		R0,		R0,		#0x1 ; set bit-0
-				STR		R0,		[R1]
+                LDR	    R1,	    =RCGCTIMER_REG
+                LDR     R0,	    [R1]
+                ORR     R0,	    R0,	    #0x1 ; set bit-0
+                STR     R0,	    [R1]
 
 				; configure timer-A,B in concatenated mode for Timer-0
-				LDR		R1,		=TIMER0CFG_REG
-				MOV		R0,		#0
-				STR		R0,		[R1]
+                LDR     R1,	    =TIMER0CFG_REG
+                MOV     R0,	    #0
+                STR     R0,	    [R1]
                                 
 				
-				; periodic mode -> 500ms countinuous delay
-				LDR		R1,		=TIMER0AMR_REG
+                ; periodic mode -> 1000ms countinuous delay
+                LDR     R1,     =TIMER0AMR_REG
                 LDR     R0,     [R1]
                 BIC     R0,     R0,     #0x3    ; clear the lower two bits
                 ORR     R0,     R0,     #0x2    ; set bit-1
-				STR		R0,		[R1]
+				STR     R0,     [R1]
 
-				; value to count for 500ms delay @16Mhz clock
-				LDR		R1,		=TIMER0AIL_REG
+                ; value to count for 1000ms delay @16Mhz clock
+                LDR		R1,		=TIMER0AIL_REG
                 LDR     R0,     =TIMER_DELAY_VAL
-				STR		R0,		[R1]
+                STR     R0,     [R1]
 				
-				; enable Interrupt for Timer-A 
-				LDR		R1,		=TIMER0IMR_REG
-				LDR		R0,		[R1]
-				ORR		R0,		R0,		#0x1 ; set bit-0
-				STR		R0,		[R1]
+                ; enable Interrupt for Timer-A 
+                LDR     R1,     =TIMER0IMR_REG
+                LDR     R0,     [R1]
+                ORR     R0,     R0,     #0x1 ; set bit-0
+                STR     R0,     [R1]
 				
                 ; 16/32-Bit Timer 0A at NVIC side
                 ; assuming the core in previllaged mode
@@ -91,22 +90,22 @@ __timer_init
                 LSLS    R2,     R2,     #19
                 STR     R2,	    [R1]                
                 
-				; enable  Timer-0 before configuration
-				LDR		R1,		=TIMER0CTL_REG
-				LDR		R0,		[R1]
+                ; enable  Timer-0 
+                LDR     R1,     =TIMER0CTL_REG
+                LDR     R0,     [R1]
                 ORR     R0,     R0,     #0x1    ; set bit-0    
-				STR		R0,		[R1]
+                STR     R0,     [R1]
 
-				BX		LR
+                BX		LR
 				
                 
 __gpio_config
 
-				; use APB bus for GPIOF
+                ; use APB bus for GPIOF
                 LDR	    R1,	    =GPIOHBCTL_REG
-				LDR	    R0,	    [R1]
-				AND	    R0,     R0,     #0x1F  ; clear bit-5
-				STR	    R0,	    [R1]
+                LDR	    R0,	    [R1]
+                AND	    R0,     R0,     #0x1F  ; clear bit-5
+                STR	    R0,	    [R1]
 				
                 ; enable clock to GPIO-F
                 LDR     R1,     =RCGC2GPIO_REG
@@ -160,7 +159,7 @@ TIMER0A_Handler PROC
 				ORR	    R0,     R0,     #0x1 ; set bit-0
 				STR	    R0,	    [R1]  
 
-				; read data register value for PF.1
+				; Toggle PF.1
                 LDR		R1,		=GPIOFDATA_APB_REG                
 				LDR		R0, 	[R1]				
 				EOR		R0, 	R0,		#0x2				
